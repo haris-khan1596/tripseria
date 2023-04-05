225cded657eb9e8a0209e8be059c07f8becedcf7
haris-khan1596 <77955039+haris-khan1596@users.noreply.github.com>
2023-04-05 14:23:26 +0500

    new additions in TripCard

--- src/App.js
+++ src/App.js
@@ -1,20 +1,79 @@
-import logo from './logo.svg';
+
 import './App.css';
 import TripCard from './components/TripCard';
 
 function App() {
   return (
-    <div className="App">
-      <TripCard
-      image1="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-      image2="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-      image3="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-      title="Example Card"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices ac nunc eu bibendum. Aliquam consectetur sapien sit amet sem aliquet, a hendrerit eros dignissim."
-    
-      />
+    <div >
+      <div className="card-grid">
+        <TripCard
+        image1="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
+        image2="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
+        image3="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
+        plannerName="plannerName"
+        From="Islamabad"
+        To="Islamabad"
+        num_days="2 days"
+        num_ppl= "40"
+        total_ppl="50"
+        rating={4.5}
+        price="5000"
+        />
+        <TripCard
+        image1="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
+        image2="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
+        image3="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
+        plannerName="plannerName"
+        From="Islamabad"
+        To="Islamabad"
+        num_days="2 days"
+        num_ppl= "40"
+        total_ppl="50"
+        rating={4.5}
+        price="5000"
+        />
+        <TripCard
+        image1="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
+        image2="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
+        image3="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
+        plannerName="plannerName"
+        From="Islamabad"
+        To="Islamabad"
+        num_days="2 days"
+        num_ppl= "40"
+        total_ppl="50"
+        rating={4.5}
+        price="5000"
+        />
+        <TripCard
+        image1="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
+        image2="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
+        image3="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
+        plannerName="plannerName"
+        From="Islamabad"
+        To="Islamabad"
+        num_days="2 days"
+        num_ppl= "40"
+        total_ppl="50"
+        rating={4.5}
+        price="5000"
+        />
+        <TripCard
+        image1="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
+        image2="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
+        image3="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
+        plannerName="plannerName"
+        From="Islamabad"
+        To="Islamabad"
+        num_days="2 days"
+        num_ppl= "40"
+        total_ppl="50"
+        rating={4.5}
+        price="5000"
+        />
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App;
--- src/components/TripCard.js
+++ src/components/TripCard.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import numeral from 'numeral';
 
 function TripCard(props) {
+    const formattedPrice = numeral(props.price).format('₨0,0.00');
+
   return (
-    <div class="card" style={{ width: '300px', height: '300px' }}>
+    <div className="card" style={{ width: '300px'}}>
       <Carousel>
         <Carousel.Item>
-          <img
-            class="d-block w-100"
-            src={props.image1}
-            alt="First slide"
-          />
+            <img className="d-block w-100" src={props.image1}  alt="First slide"/>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            class="d-block w-100"
+            className="d-block w-100"
             src={props.image2}
             alt="Second slide"
           />
@@ -27,12 +28,25 @@ function TripCard(props) {
           />
         </Carousel.Item>
       </Carousel>
-      <div class="card-body">
-        <h5 class="card-title">{props.title}</h5>
-        <p class="card-text">{props.description}</p>
+      <div className="card-info">
+        <h3>{props.plannerName}</h3>
+        <div className='details'>
+          <p >From: {props.From}</p>
+          <p className='days'>{props.num_days}</p>
+          <p>To: {props.To}</p>
+          <p className='ppl'>{props.num_ppl}/{props.total_ppl} seats</p>
+        </div>
+        <hr></hr>
+        <div className="card-rating">
+            <FontAwesomeIcon icon={faStar} className="star" />
+            <span className="rating">{props.rating}</span>
+        </div>
+        <div> {formattedPrice}</div>
+
+        <button>View Plan</button>
       </div>
     </div>
   );
 }
 
-export default TripCard;
+export default TripCard;
