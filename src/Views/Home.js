021efb73b8e160aeff856d9dffdf35e815292447
haris-khan1596 <77955039+haris-khan1596@users.noreply.github.com>
2023-04-17 00:42:49 +0500

    customizing Navbar and improving home screen

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -4,14 +4,141 @@ import Navbar from '../components/Navbar';
 
 function Home(){
     return(
-        <div>
-    <div>
+        <div >
+    <div >
         <Navbar /></div>
+        <div style={{
+          display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%"
+      }}>
+          <h2 >Trips</h2>
+        </div>
+\
+        
         <div className="card-grid">
           <TripCard
-          image1="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image2="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image3="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
+          image1="https://mir-s3-cdn-cf.behance.net/project_modules/1400/35ee6f84992701.5d6e870eac299.jpg"
+          image2="https://mir-s3-cdn-cf.behance.net/project_modules/1400/9549e184992701.5d735663624f5.jpg"
+          image3="https://mir-s3-cdn-cf.behance.net/project_modules/1400/fd9ac684992701.5d6e870eac734.jpg"
+          plannerName="Clut Trips"
+          From="Islamabad"
+          To="Amazin"
+          num_days="2 days"
+          num_ppl= "40"
+          total_ppl="50"
+          rating={4.5}
+          price="5000"
+          />
+          <TripCard
+          image1="https://mir-s3-cdn-cf.behance.net/project_modules/1400/1d76f4167506323.642a8efcdabd0.jpg"
+          image2="https://mir-s3-cdn-cf.behance.net/project_modules/1400/28b7c3167506323.642a8efcdd2f2.jpg"
+          image3="https://mir-s3-cdn-cf.behance.net/project_modules/fs/d1755a167506323.642a8efcd9088.jpg"
+          plannerName="NorthX"
+          From="Islamabad"
+          To="Kashmir"
+          num_days="2 days"
+          num_ppl= "40"
+          total_ppl="50"
+          rating={4.5}
+          price="5000"
+          />
+          <TripCard
+          image1="https://mir-s3-cdn-cf.behance.net/project_modules/1400/472a3191211895.5e2b7eee6d0de.jpg"
+          image2="https://mir-s3-cdn-cf.behance.net/project_modules/1400/046c5391211895.5e2b7eee6ddb6.jpg"
+          image3="https://mir-s3-cdn-cf.behance.net/project_modules/1400/aba85091211895.5e2b7eee6f166.jpg"
+          plannerName="plannerName"
+          From="Islamabad"
+          To="Islamabad"
+          num_days="2 days"
+          num_ppl= "40"
+          total_ppl="50"
+          rating={4.5}
+          price="5000"
+          />
+          <TripCard
+          image1="https://mir-s3-cdn-cf.behance.net/project_modules/1400/ff1bf2162893975.63dcd79ac1781.jpg"
+          image2="https://mir-s3-cdn-cf.behance.net/project_modules/1400/4db3bf162893975.63dcd79ab9234.jpg"
+          image3="https://mir-s3-cdn-cf.behance.net/project_modules/1400/8b21a1162893975.63dcd79abb10c.jpg"
+          plannerName="plannerName"
+          From="Islamabad"
+          To="Muree"
+          num_days="2 days"
+          num_ppl= "40"
+          total_ppl="50"
+          rating={4.5}
+          price="5000"
+          />
+          <TripCard
+          image1="https://mir-s3-cdn-cf.behance.net/project_modules/fs/5b0581148756071.62dacebb55f89.jpg"
+          image2="https://mir-s3-cdn-cf.behance.net/project_modules/1400/b3a252148756071.62dacebb59fa5.jpg"
+          image3="https://mir-s3-cdn-cf.behance.net/project_modules/1400/584acc148756071.62dacebb59314.jpg"
+          plannerName="plannerName"
+          From="Islamabad"
+          To="Islamabad"
+          num_days="2 days"
+          num_ppl= "40"
+          total_ppl="50"
+          rating={4.5}
+          price="5000"
+          />
+          <TripCard
+          image1="https://mir-s3-cdn-cf.behance.net/project_modules/1400/472a3191211895.5e2b7eee6d0de.jpg"
+          image2="https://mir-s3-cdn-cf.behance.net/project_modules/1400/046c5391211895.5e2b7eee6ddb6.jpg"
+          image3="https://mir-s3-cdn-cf.behance.net/project_modules/1400/aba85091211895.5e2b7eee6f166.jpg"
+          plannerName="plannerName"
+          From="Islamabad"
+          To="Islamabad"
+          num_days="2 days"
+          num_ppl= "40"
+          total_ppl="50"
+          rating={4.5}
+          price="5000"
+          />
+          <TripCard
+          image1="https://mir-s3-cdn-cf.behance.net/project_modules/1400/1d76f4167506323.642a8efcdabd0.jpg"
+          image2="https://mir-s3-cdn-cf.behance.net/project_modules/1400/28b7c3167506323.642a8efcdd2f2.jpg"
+          image3="https://mir-s3-cdn-cf.behance.net/project_modules/fs/d1755a167506323.642a8efcd9088.jpg"
+          plannerName="NorthX"
+          From="Islamabad"
+          To="Kashmir"
+          num_days="2 days"
+          num_ppl= "40"
+          total_ppl="50"
+          rating={4.5}
+          price="5000"
+          />
+          <TripCard
+          image1="https://mir-s3-cdn-cf.behance.net/project_modules/1400/35ee6f84992701.5d6e870eac299.jpg"
+          image2="https://mir-s3-cdn-cf.behance.net/project_modules/1400/9549e184992701.5d735663624f5.jpg"
+          image3="https://mir-s3-cdn-cf.behance.net/project_modules/1400/fd9ac684992701.5d6e870eac734.jpg"
+          plannerName="Clut Trips"
+          From="Islamabad"
+          To="Amazin"
+          num_days="2 days"
+          num_ppl= "40"
+          total_ppl="50"
+          rating={4.5}
+          price="5000"
+          />
+          <TripCard
+          image1="https://mir-s3-cdn-cf.behance.net/project_modules/1400/1d76f4167506323.642a8efcdabd0.jpg"
+          image2="https://mir-s3-cdn-cf.behance.net/project_modules/1400/28b7c3167506323.642a8efcdd2f2.jpg"
+          image3="https://mir-s3-cdn-cf.behance.net/project_modules/fs/d1755a167506323.642a8efcd9088.jpg"
+          plannerName="NorthX"
+          From="Islamabad"
+          To="Kashmir"
+          num_days="2 days"
+          num_ppl= "40"
+          total_ppl="50"
+          rating={4.5}
+          price="5000"
+          />
+          <TripCard
+          image1="https://mir-s3-cdn-cf.behance.net/project_modules/fs/5b0581148756071.62dacebb55f89.jpg"
+          image2="https://mir-s3-cdn-cf.behance.net/project_modules/1400/b3a252148756071.62dacebb59fa5.jpg"
+          image3="https://mir-s3-cdn-cf.behance.net/project_modules/1400/584acc148756071.62dacebb59314.jpg"
           plannerName="plannerName"
           From="Islamabad"
           To="Islamabad"
@@ -35,116 +162,12 @@ function Home(){
           price="5000"
           />
           <TripCard
-          image1="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image2="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image3="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          plannerName="plannerName"
+          image1="https://mir-s3-cdn-cf.behance.net/project_modules/1400/1d76f4167506323.642a8efcdabd0.jpg"
+          image2="https://mir-s3-cdn-cf.behance.net/project_modules/1400/28b7c3167506323.642a8efcdd2f2.jpg"
+          image3="https://mir-s3-cdn-cf.behance.net/project_modules/fs/d1755a167506323.642a8efcd9088.jpg"
+          plannerName="NorthX"
           From="Islamabad"
-          To="Islamabad"
-          num_days="2 days"
-          num_ppl= "40"
-          total_ppl="50"
-          rating={4.5}
-          price="5000"
-          />
-          <TripCard
-          image1="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image2="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image3="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          plannerName="plannerName"
-          From="Islamabad"
-          To="Islamabad"
-          num_days="2 days"
-          num_ppl= "40"
-          total_ppl="50"
-          rating={4.5}
-          price="5000"
-          />
-          <TripCard
-          image1="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image2="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image3="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          plannerName="plannerName"
-          From="Islamabad"
-          To="Islamabad"
-          num_days="2 days"
-          num_ppl= "40"
-          total_ppl="50"
-          rating={4.5}
-          price="5000"
-          />
-          <TripCard
-          image1="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image2="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image3="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          plannerName="plannerName"
-          From="Islamabad"
-          To="Islamabad"
-          num_days="2 days"
-          num_ppl= "40"
-          total_ppl="50"
-          rating={4.5}
-          price="5000"
-          />
-          <TripCard
-          image1="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image2="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image3="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          plannerName="plannerName"
-          From="Islamabad"
-          To="Islamabad"
-          num_days="2 days"
-          num_ppl= "40"
-          total_ppl="50"
-          rating={4.5}
-          price="5000"
-          />
-          <TripCard
-          image1="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image2="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image3="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          plannerName="plannerName"
-          From="Islamabad"
-          To="Islamabad"
-          num_days="2 days"
-          num_ppl= "40"
-          total_ppl="50"
-          rating={4.5}
-          price="5000"
-          />
-          <TripCard
-          image1="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image2="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image3="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          plannerName="plannerName"
-          From="Islamabad"
-          To="Islamabad"
-          num_days="2 days"
-          num_ppl= "40"
-          total_ppl="50"
-          rating={4.5}
-          price="5000"
-          />
-          <TripCard
-          image1="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image2="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image3="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          plannerName="plannerName"
-          From="Islamabad"
-          To="Islamabad"
-          num_days="2 days"
-          num_ppl= "40"
-          total_ppl="50"
-          rating={4.5}
-          price="5000"
-          />
-          <TripCard
-          image1="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image2="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          image3="https://guidetopakistan.pk/wp-content/uploads/2021/11/Pak.Monument-petals3.jpg"
-          plannerName="plannerName"
-          From="Islamabad"
-          To="Islamabad"
+          To="Kashmir"
           num_days="2 days"
           num_ppl= "40"
           total_ppl="50"
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Nav, Navbar, Button } from 'react-bootstrap';
+import { Nav, Button } from 'react-bootstrap';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 
 function NavBar(){
   return (
-    <Navbar className='navbar'>
-      <Navbar.Brand href="/" className='navbar-logo'>Tripseria</Navbar.Brand>
-      
-        <Nav className="navbar-items">
+    <div style={{
+      paddingBottom: "85px"
+      }}>
+        
+    <Navbar key="lg" bg="light" expand="lg" className="mb-3 fixed-top">
+    <Container fluid>
+     <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
+      <Navbar.Brand href="/" className='navbar-logo'><h2 style={{
+        paddingLeft: "100px"
+        }}>Tripserio</h2></Navbar.Brand>
+       <Navbar.Offcanvas
+id={`offcanvasNavbar-expand-lg`}
+aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+placement="end"
+>
+     <Offcanvas.Header closeButton>
+ <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
+Offcanvas
+</Offcanvas.Title>
+</Offcanvas.Header>
+<Offcanvas.Body>
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="justify-content-center flex-grow-1 pe-3">
+
           <Nav.Link as={Link} to="/recommended" className='navbar-item'>Recommended Trips</Nav.Link>
           <Nav.Link as={Link} to="/hot" className='navbar-item'>Hot Trips</Nav.Link>
           <Nav.Link as={Link} to="/new" className='navbar-item'>New Trips</Nav.Link>
           <Nav.Link as={Link} to="/favourites" className='navbar-item'>Favourite Trips</Nav.Link>
           <Nav.Link as={Link} to="/mytrips" className='navbar-item'>My Trips</Nav.Link>
-        </Nav>
+          <span></span>
+          </Nav>
+          
       <Button variant="primary" as={Link} to="/create-trip" className='navbar-create-btn'>Create Your Own Trip</Button>
-      
-    </Navbar>
+      </Navbar.Collapse>
+      </Offcanvas.Body></Navbar.Offcanvas></Container>
+    </Navbar></div>
   );
 };
 
 export default NavBar;
+
