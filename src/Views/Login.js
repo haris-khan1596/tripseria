033b0f9dfc0143501b7dfd18f90c8ba44fcf03ba
haris-khan1596 <77955039+haris-khan1596@users.noreply.github.com>
2023-05-08 20:39:32 +0500

    changes in login page

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,43 +1,81 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import "./CSS/Login.css"; // import CSS file
+// import { Link } from "react-router-dom";
+
+// function Login() {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     console.log(`Email: ${email}, Password: ${password}`);
+//     // Call API endpoint to verify login details
+//   };
+
+//   return (
+//     <div className="login-container">
+      
+//       <div className="form-container">
+//         <div className="logo-container">
+//           {/*<img src="https://www.example.com/tripserio-logo.png" alt="Tripserio" className="logo" />*/}
+//           <p className="logo">Tripserio</p>
+//         </div>
+//         <h1>Login to your Tripserio account</h1>
+//         <form onSubmit={handleSubmit}>
+//           <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+//           <br />
+//           <label>
+//             Password:
+//             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+//           </label>
+//           <br />
+//           <button type="submit">Login</button>
+//         </form>
+//         <p>Don't have an account? <Link to="/signup">Sign up</Link></p>
+//       </div>
+//       <div className="image-container">
+//         <img src={process.env.PUBLIC_URL +"Assets/login.svg"} alt="Travel" />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Login;
+import React, { useState } from 'react';
 import "./CSS/Login.css"; // import CSS file
-import { Link } from "react-router-dom";
 
 function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(`Email: ${email}, Password: ${password}`);
-    // Call API endpoint to verify login details
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
   };
 
   return (
+    <div>
     <div className="login-container">
-      <div className="image-container">
-        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/d1755a167506323.642a8efcd9088.jpg" alt="Travel" />
+      <div className="login-logo">
+        <img src="logo.png" alt="Logo" />
       </div>
-      <div className="form-container">
-        <div className="logo-container">
-          {/*<img src="https://www.example.com/tripserio-logo.png" alt="Tripserio" className="logo" />*/}
-          <p className="logo">Tripserio</p>
+      <div className="login-form">
+        <h2>Welcome back!</h2>
+        <p>Login to continue</p>
+        <div className="input-container">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" value={username} onChange={handleUsernameChange} placeholder="abc@gmail.com" required />
         </div>
-        <h1>Login to your Tripserio account</h1>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Email:
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-          </label>
-          <br />
-          <label>
-            Password:
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          </label>
-          <br />
-          <button type="submit">Login</button>
-        </form>
-        <p>Don't have an account? <Link to="/signup">Sign up</Link></p>
+        <div className="input-container">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" value={password} onChange={handlePasswordChange} placeholder="******" required />
+        </div>
+        <button type="submit">Login</button>
       </div>
+    </div>
     </div>
   );
 }
