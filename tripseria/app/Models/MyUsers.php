a9d8b3a5e77c55b9ab0fa6cc4f5da65cea7b897a
haris-khan1596 <77955039+haris-khan1596@users.noreply.github.com>
2023-05-16 12:45:52 +0500

    Adding laravel Project for Backend

---
+++ tripseria/app/Models/MyUsers.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MyUsers extends Model
+{
+    use HasFactory;
+}
