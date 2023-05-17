96a56295517facd89bbc3f7e29f248d9e8466553
haris-khan1596 <77955039+haris-khan1596@users.noreply.github.com>
2023-05-17 23:58:34 +0500

    update index

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route ,Navigate} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -13,10 +13,16 @@ import Home from './Views/Home';
 import Layout from './components/Layouts/Layout';
 import TripList  from './Views/TripList';
 import TravelPlanner from './Views/createTrip';
-import UserProf from './Views/UserProfile';
+import { isloggedIn, isplanner } from './helper';
+//import UserProf from './Views/UserProfile';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+localStorage.setItem('token', "xjibfibuhfhbjnfnuzxnigkfdi");
+localStorage.setItem('isloggedIn',true);
+localStorage.setItem('isplanner',false);
+localStorage.setItem('user',null);
+localStorage.setItem('api',"http://10.7.92.226:8000/api");
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -24,12 +30,14 @@ root.render(
     
     <Route path="/" element={<Layout><Ghome /></Layout>} />
     <Route path='/trips' element={<Layout><br/><br/><br/><Home /></Layout>}/>
-    <Route path="/login" element={<Login/>}/>
-    <Route path="/signup" element ={<SignUp/>}/>
+    <Route path="/login" element={isloggedIn()?isplanner()?<Navigate to='/planner/profile' />:<Navigate to='/user/profile' />:<Login/>}/>
+    <Route path="/signup" element ={isloggedIn()?isplanner()?<Navigate to='/planner/profile' />:<Navigate to='/user/profile' />:<SignUp/>}/>
     <Route path='/search' element={<TripList/>}/>
-    <Route path='/planner/signup' element={<SignUp />}/>
-    <Route path='/planner/trips' element={<TravelPlanner />}/>
-    <Route path='/user/profile' element={<UserProf />}/>
+    <Route path='/planner/signup' element={isloggedIn()?isplanner()?<Navigate to='/planner/profile' />:<Navigate to='/user/profile' />:<SignUp />}/>
+    <Route path='/planner/trips' element={isloggedIn()&&isplanner() ?<TravelPlanner />:<Navigate to='/planner/signup' />}/>
+
+    <Route path='*' element={<Navigate to='/' />}/>
+  {/* <Route path='/user/profile' element={<UserProf />}/> */}
     </Routes>
     </BrowserRouter>
 
