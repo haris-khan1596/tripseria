d69e5c1b33afec503f369b30ad6f60e2ddb8dbae
haris-khan1596 <77955039+haris-khan1596@users.noreply.github.com>
2023-05-17 15:43:28 +0500

    configuring backend with frontend successfully

--- tripseria/routes/web.php
+++ tripseria/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MyUserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/signup', function () {
+    return view('signup');
+})->name('signup');
+Route::post('/signup/{id}', [MyUserController::class , 'signup']);
