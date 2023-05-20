273e0b93e2782716e9343cef971e496c483c1b8e
Manahil Ashraf <manahilghumman10@gmail.com>
2023-05-21 00:10:11 +0500

    PlannerTrips complete

--- Frontend/src/Views/PlannerTrips.js
+++ Frontend/src/Views/PlannerTrips.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import axios from 'axios';
 import React, { useState } from "react";
+import "./CSS/PlannerTrips.css";
 
 function PlannerTrips() {
     const [trips, setTrips] = useState([]);
@@ -18,25 +19,33 @@ function PlannerTrips() {
       });
 
     return(
-        <div>
+        <div className="planner-trips"> 
             {trips.length===0? (
         <p>Loading...</p>
       ) : (
-        <table>
-        {trips.map((trip) => (
+        <table className="trips-table">
+            <thead>
             <tr>
-                <td>
-                    {trip.from}
-                </td>
-                <td>
-                    {trip.to}
-                </td>
-                <td>
-                    <a href={localStorage.getItem('api')+'planner/trips/'+trip.id+'/destroy'}><button className="btn btn-danger">Delete</button></a>
-                </td>
+              <th>From</th>
+              <th>To</th>
+              <th>Price</th>
+              <th>Number of People</th>
+              <th>Action</th>
             </tr>
-
-      ))}
+            </thead>
+            <tbody>
+                {trips.map((trip) => (
+                    <tr key={trip.id}>
+                        <td>{trip.from}</td>
+                        <td>{trip.to}</td>
+                        <td>{trip.price}</td>
+                        <td>{trip.num_ppl}</td>
+                        <td>
+                            <a href={localStorage.getItem('api')+'planner/trips/'+trip.id+'/destroy'}><button className="btn btn-danger delete-btn">Delete</button></a>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
       </table>
       )}
         </div>
